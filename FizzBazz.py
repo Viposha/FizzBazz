36873a0b1e9c7111efcaa0afc28327db966ed991
Vitalii Hoshchenko <viposha@gmail.com>
2022-04-17 12:39:14 +0300

    write script fizzbazz

--- FizzBazz.py
+++ FizzBazz.py
@@ -1,7 +1,15 @@
 # start developing
 
 def fizzbazz():
-    pass
+    for i in range(1, 100+1):
+        if i % 3:
+            print("Fizz")
+        elif i % 5:
+            print("Buzz")
+        elif i % 3 and i % 5:
+            print("FizzBazz"
+        else:
+            print(i)
 
 
 fizzbazz()
