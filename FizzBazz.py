acbfd44f1a528a81c2d209477aa6d6578fa12166
Vitalii Hoshchenko <viposha@gmail.com>
2022-04-17 16:04:03 +0300

    Fixed critical bug in FizzBazz.py

--- FizzBazz.py
+++ FizzBazz.py
@@ -2,12 +2,12 @@
 
 def fizzbazz():
     for i in range(1, 100+1):
-        if i % 3:
+        if i % 3 == 0 and not i % 5 == 0:
             print("Fizz")
-        elif i % 5:
+        elif i % 5 == 0 and not i % 3 == 0 :
             print("Buzz")
-        elif i % 3 and i % 5:
-            print("FizzBazz"
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBazz")
         else:
             print(i)
 
